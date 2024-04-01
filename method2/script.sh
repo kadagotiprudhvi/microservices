1032dd130ce6628a6a7502e55edfcd3d3aa538d2
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-01 14:11:15 +0530

    Update script.sh

--- method2/script.sh
+++ method2/script.sh
@@ -93,7 +93,27 @@ fi
 
 
 read "bash final_script--help"
-echo "    "
+echo " To use the script, follow the instructions below:
+
+1)This command navigates to correct directory path of the file
+
+$ cd Microservices/method2/
+2)This command runs the script and deploys the service
+
+$ bash root.sh
+After running the above command enter the inputs with appropriate parameters as below mentioned
+
+Enter the input environtment to install the microservices
+•aws
+•container
+
+Enter the input microservice_name to install
+•demo-frontend
+•demo-backendend1
+•demo-backendend2
+
+If you select aws it asks you to enter ip address
+•Ip_address   "
 
 
 
